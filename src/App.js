6b3063e1a392a5e140c31378ea4e92921a77644b
Holly Blevins <Holly@macbook-pro.mynetworksettings.com>
2024-07-28 12:50:52 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+// useEffect & useState are hooks
 
 function App() {
+
+  // create a variable in state called "count" with initial value of 0
+    // ex: const [state variable, how you update state variable] = useState
+  const [count, setCount] = useState(0);
+  // count is set to equal 0
+  // goal is to increment value by 1
+
+  // everytime the "count" is updated, log a message to the console
+  useEffect(() => {
+    console.log('count was updated')
+  }, [count]); 
+
+  const incrementCount = () => {
+    setCount(count + 1); // use this code to "paint" (render) output onto screen instead of console
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type='button' onClick={incrementCount}>+</button>
+      {/* attribute of button doesn't need a > sign until after attribute is assigned? */}
+        {/* ex: <button type=''> */}
+      {/* display value of "count" with {} */}
+      <div>{count}</div>
+      {/* code written above puts the value on a different line */}
     </div>
   );
 }
